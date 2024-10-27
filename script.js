5046c5739d92c1fc7f01d8b54c0b9af9ec4c74ee
Stakshi <128694806+Stakshi301@users.noreply.github.com>
2024-10-27 14:38:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+const itemList = document.getElementById('colorSelect');
+const deleteButton = document.querySelector('input[type="button"]');
+
+    deleteButton.addEventListener('click', function() {
+        // Get the selected option
+        const selectedOption = itemList.options[itemList.selectedIndex];
+
+        // If there is a selected option, remove it
+        if (selectedOption) {
+            itemList.removeChild(selectedOption);
+        }
+});
